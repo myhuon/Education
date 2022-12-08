a4f5ef64b68ee7838a995468ca4ed7c26a4b8c9b
myhuon <myhuon@naver.com>
2022-12-08 10:10:06 +0900

    [Lightning Component] accDisplay.cmp, accEdit.cmp, accEditController.js

---
+++ src/aura/accDisplay/accDisplayController.js
@@ -0,0 +1,13 @@
+/**
+ * @description       : 
+ *                      
+ * @author            : hyunsoo.song@daeunextier.com
+ * @group             :
+ * @last modified on  : 2022-12-07
+ * @last modified by  : hyunsoo.song@daeunextier.com
+ * Modifications Log
+ * Ver     Date             Author               Modification
+ * 1.0   2022-12-07   hyunsoo.song@daeunextier.com   Initial Version
+ */
+({
+});
---
+++ src/aura/accEdit/accEditController.js
@@ -0,0 +1,28 @@
+/**
+ * @description       : 
+ *                      
+ * @author            : hyunsoo.song@daeunextier.com
+ * @group             :
+ * @last modified on  : 2022-12-07
+ * @last modified by  : hyunsoo.song@daeunextier.com
+ * Modifications Log
+ * Ver     Date             Author               Modification
+ * 1.0   2022-12-07   hyunsoo.song@daeunextier.com   Initial Version
+ */
+({
+
+    handleSaveRecord: function(component, event, helper) {
+        component.find("recordEditor").saveRecord($A.getCallback(function(saveResult) {
+            if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+                console.log("Save completed successfully.");
+            } else if (saveResult.state === "INCOMPLETE") {
+                console.log("User is offline, device doesn't support drafts.");
+            } else if (saveResult.state === "ERROR") {
+                console.log('Problem saving record, error: ' +
+                JSON.stringify(saveResult.error));
+            } else {
+                console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+            }
+    }));}
+
+});
